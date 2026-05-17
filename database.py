9c2d26ae2605a7bf2fe971b7945399fb19b4102f
quare-albus <166514931+quare-albus@users.noreply.github.com>
2026-05-17 12:04:27 +0530

    FIX Database connection

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///planner.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
